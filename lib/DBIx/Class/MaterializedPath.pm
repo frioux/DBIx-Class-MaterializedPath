a2fdcae20f4b03bbef8e4eb159b49065a114e063
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-06-28 21:32:58 -0500

    initial doc

--- lib/DBIx/Class/MaterializedPath.pm
+++ lib/DBIx/Class/MaterializedPath.pm
@@ -181,3 +181,134 @@ sub _install_reverse_full_path_rel {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+ package A::Schema::Result::Category;
+
+ use strict;
+ use warnings;
+
+ use base 'DBIx::Class::Core';
+
+ __PACKAGE__->table('category');
+
+ __PACKAGE__->load_components('MaterializedPath');
+
+ __PACKAGE__->add_columns(
+    id => {
+       data_type => 'int',
+       is_auto_increment => 1,
+    },
+
+    parent_id => {
+       data_type => 'int',
+       is_nullable => 1, # root
+    },
+
+    parent_path => {
+       data_type => 'varchar',
+       size      => 256,
+       is_nullable => 1,
+    },
+
+    name => {
+       data_type => 'varchar',
+       size      => 256,
+    },
+ );
+
+ __PACKAGE__->set_primary_key('id');
+
+ __PACKAGE__->belongs_to(
+   parent_category => 'A::Schema::Result::Category', 'parent_id'
+ );
+
+ __PACKAGE__->has_many(
+   child_categories => 'A::Schema::Result::Category', 'parent_id'
+ );
+
+ sub materialized_path_columns {
+    return {
+       parent => {
+          parent_column                => 'parent_id',
+          parent_fk_column             => 'id',
+          materialized_path_column     => 'parent_path',
+          include_self_in_path         => 1,
+          include_self_in_reverse_path => 1,
+          separator                    => '/',
+          parent_relationship          => 'parent_category',
+          children_relationship        => 'child_categories',
+          full_path                    => 'ancestors',
+          reverse_full_path            => 'descendants',
+       },
+    }
+ }
+
+ 1;
+
+Elsewhere...
+
+ my $child_rows = $row->descendants;
+
+or better yet
+
+ my $awesome_kids = $rs->search({ awesome => 1 })
+   ->related_resultset('descendants');
+
+=head1 DESCRIPTION
+
+L<Materialized path|https://communities.bmc.com/communities/docs/DOC-9902> is a
+way to store trees in relational databases that results in very efficient
+retrieval, at the expense of space and more write-time queries.
+
+This module makes using matpaths easy.  The interface is somewhat unusual, but
+the benefit is that it creates actual relationships for the both directions of
+the tree, allowing you to use the powerful querying L<DBIx::Class> already gives
+you.
+
+The first strange part of the interface is that the call to C<load_components>
+B<must> come after the call to C<table>.  The next strange bit is that the way
+you define all of the metadata about your matpath is by defining a subroutine
+called C<materialized_path_columns>.  The subroutine must return a hashref
+where the key is name of your path (currently unused) and the value is the
+metadata for the path.  Here are the parts that need to be defined in the
+metadata:
+
+=over 2
+
+=item * C<parent_column> - the column that points directly to the parent row,
+for example C<parent_id>.
+
+=item * C<parent_fk_column> - the column that C<parent_column> points to, for
+example C<id>.
+
+=item * C<materialized_path_column> - the column that contains the materialized
+path.  One thing to note here is that the B<width> of the column defines how
+deep your tree can be.  For example if the number of digits for most of your
+C<id>'s is three, and your materialized path column is 255 characters wide, you
+can support a depth of something like 63 levels, because each level in the tree
+is C<< q(/) . "$id" >>, so C<< 255/4 ~~ 63 >>.  An example for this might be
+C<parent_materialized_path>.
+
+=item * C<parent_relationship> - the direct relationship to your parent row
+
+=item * C<children_relationship> - the direct relationship to your child rows
+
+=item * C<full_path> - the name of the relationship that this materialized path
+will define for all parents.  For example, C<ancestry>.
+
+=item * C<reverse_full_path> - the name of the relationship that this
+materialized path will define for all children.  For example, C<descendants>.
+
+=item * C<include_self_in_path> - (optional) I've found that it's often
+helpful to include the current row in the full ancestry.  For example if
+your path is a for categorization system you probably want to turn this on.
+
+=item * C<include_self_in_reverse_path> - (optional) see above, but for chilren
+instead of parents
+
+=item * C<separator> - (optional) defaults to C</>.  If you don't know what
+this is for just ignore it.
+
+=back
